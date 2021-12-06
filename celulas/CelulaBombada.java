ad2541b3deab0e7d360a5f4f0d0f67447660aa32
Maria Fernanda Rodrigues Vieira e Silva <93397962+Nandrigues@users.noreply.github.com>
2021-12-06 11:28:56 -0300

    Add files via upload

---
+++ celulas/CelulaBombada.java
@@ -0,0 +1,11 @@
+package celulas;
+
+public class CelulaBombada extends Celula {
+	
+	public CelulaBombada(){ // Setando a bomba
+		super.setBombado(true);
+			
+			
+	}
+
+}
---
+++ celulas/CelulaBranca.java
@@ -0,0 +1,7 @@
+package celulas;
+
+public class CelulaBranca extends Celula{
+	public CelulaBranca() {
+		super.setVizinhosbomba(0); 
+	}
+}
