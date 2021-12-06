9a4b629c562264518f3765390e824d9a604679c4
Maria Fernanda Rodrigues Vieira e Silva <93397962+Nandrigues@users.noreply.github.com>
2021-12-06 11:18:28 -0300

    Delete Constantes.java

--- Constantes.java
+++
@@ -1,9 +0,0 @@
-package campominado;
-
-public interface Constantes {
-	
-	public static final int LINHAS = 8; 
-	public static final int COLUNAS = 8; 
-	public static final int NUMINAS = 8;
-	public static final int NUMALUCAS = 8;
-}
