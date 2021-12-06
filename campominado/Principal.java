6a924c7c22fad2c2352929d613c1dcdf27d9420f
GPoroca <93682321+GPoroca@users.noreply.github.com>
2021-12-06 12:06:07 -0300

    Add files via upload

--- campominado/Principal.java
+++ campominado/Principal.java
@@ -1,12 +1,16 @@
 package campominado;
 
+import arquivosetempo.Cronometro;
+
 public class Principal {
 	public static void main(String[] args) throws InterruptedException{
 		Consoleui console = new Consoleui();
+		Cronometro cronometro = new Cronometro();
 
 		console.modoDeJogo();
 		console.clearScreen();
 		console.printarCampo();
+		cronometro.start();
 			do {
 					
 				console.lerAcao();
@@ -15,6 +19,8 @@ public class Principal {
 				console.printarCampo();
 				
 			} while (console.getrodar() == true);
+			cronometro.stop();
+			System.out.println("Tempo: " + cronometro.getElapsedTimeSecs() + " segundos" );
 		}
 }
 
